597d6e1a6f4ad9a7fe7518eed33f6758b5f5b015
sharkpause <carlmabfredishere@gmail.com>
2025-11-19 14:47:47 +0700

    feat: change to make edit and delete button appear on hover

--- dsa_visualize/component/dsa/linkedlist/LinkedListVisualizer.tsx
+++ dsa_visualize/component/dsa/linkedlist/LinkedListVisualizer.tsx
@@ -7,7 +7,7 @@ import { LinkedList } from '@/lib/data_structures/linkedlist/LinkedList';
 function makeSampleLinkedList(): LinkedList {
     const list: LinkedList = new LinkedList();
     
-    for(let i = 10; i <= 50; i += 10) {
+    for(let i = 10; i <= 30; i += 10) {
         list.add_node(i);
     }
 
@@ -15,23 +15,40 @@ function makeSampleLinkedList(): LinkedList {
 }
 
 export default function LinkedListVisualizer() {
-    const [sampleLinkedList, setSampleLinkedList] = useState(() => makeSampleLinkedList());
+    const [list, setSampleLinkedList] = useState(() => makeSampleLinkedList());
     
     function handlePlusButtonClick() {
-        const clonedList = sampleLinkedList.clone();
+        const clonedList = list.clone();
         clonedList.add_node(Math.floor(Math.random() * 100));
         
         setSampleLinkedList(clonedList);
     }
 
+    function handleEditButtonClick(index: number) {
+        const clonedList = list.clone();
+        alert(index)
+    }
+
+    function handleDeleteButtonClick(index: number) {
+        const clonedList = list.clone();
+        alert(index);
+        // clonedList.delete_node()
+    }
+
     return <div>
         <div>
             {
-                sampleLinkedList.to_array().map((value, idx) => (
-                    <span key={idx}>
-                        <span>{value}</span>
-                        {' → '}
-                    </span>
+                list.to_array().map((value, idx) => (
+                    <div key={idx} className="group relative inline-block">
+                      <div className="px-4 py-2 bg-white border rounded-md shadow-sm">
+                        {value}
+                      </div>
+
+                      <div className="mt-3 absolute bg-gray-100 p-2 opacity-0 group-hover:opacity-100 transition-opacity">
+                        <button onClick={() => handleEditButtonClick(idx)} className="py-1">Edit</button>
+                        <button onClick={() => handleDeleteButtonClick(idx)} className="py-1 text-red-600">Delete</button>
+                      </div>
+                    </div>
                 ))
             }
             <button className="cursor-pointer border-solid border-black outline p-1" onClick={handlePlusButtonClick}>
