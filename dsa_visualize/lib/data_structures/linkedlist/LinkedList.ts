846db737082d865ed63a1980baade43b2c7fdaba
sharkpause <carlmabfredishere@gmail.com>
2025-11-19 14:56:11 +0700

    feat: made delete_at method and added a unique id for each node

--- dsa_visualize/lib/data_structures/linkedlist/LinkedList.ts
+++ dsa_visualize/lib/data_structures/linkedlist/LinkedList.ts
@@ -1,6 +1,7 @@
 export class ListNode {
     value: number;
     next: ListNode | null;
+    id = crypto.randomUUID()
 
     constructor(value: number, next: ListNode | null = null) {
         this.value = value;
@@ -11,13 +12,14 @@ export class ListNode {
 export class LinkedList {
     head: ListNode | null;
     tail: ListNode | null;
+    length: number = 0;
     
     constructor(head: ListNode | null = null) {
         this.head = head;
         this.tail = head;
     }
 
-    to_array(): number[] { // 10 20 30 40
+    to_array(): number[] { // TODO: Turn this to return a node objecct instead of just the value
         if(this.head == null) {
             return []
         }
@@ -34,6 +36,8 @@ export class LinkedList {
     }
 
     add_node(value: number): number {
+        this.length++;
+        
         if(this.head == null) {
             this.head = new ListNode(value)
             this.tail = this.head
@@ -56,6 +60,19 @@ export class LinkedList {
         }
 
         return newList;
+    }
+
+    delete_at(index: number) {
+        if(this.head == null || index >= this.length) {
+            return 1;
+        }
+        
+        let currentNode: ListNode | null = this.head;
+        let i = 0;
+        
+        while(i < index) {
+            currentNode = currentNode!.next
+        }
     }
 }
 
